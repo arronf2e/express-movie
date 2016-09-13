8f321e5567514d6ed63a420ede0bd6cffbaab5cc
arron <191446367@qq.com>
2016-09-13 10:40:09 +0800

    version1.0 is completed

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Movie = require('./models/movie');
 var _ = require('underscore');
+var moment = require('moment');
 
 mongoose.connect('mongodb://localhost/movie')
 app.set('views', './views/pages');
 app.set('view engine', 'jade');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port);
 console.log('Server is on port 3000!');
+app.locals.moment = moment;
 
 // index page
 app.get('/', function(req, res) {
@@ -57,6 +59,19 @@ app.get('/admin/movie', function(req, res) {
     })
 });
 
+// admin update movie
+app.get('/admin/update/:id', function(req, res) {
+    var id = req.params.id
+    if (id) {
+        Movie.findById(id, function(err, movie) {
+            res.render('admin', {
+                title: 'movie admin',
+                movie: movie
+            })
+        })
+    }
+})
+
 // admin post movie
 app.post('/admin/movie/new', function(req, res) {
     var id = req.body.movie._id;
@@ -67,24 +82,61 @@ app.post('/admin/movie/new', function(req, res) {
             if (err) {
                 console.log(err)
             }
+            _movie = _.extend(movie, movieObj);
+            _movie.save(function(err, movie) {
+                if (err) {
+                    console.log(err)
+                }
+
+                res.redirect('/movie/' + movie._id);
+            })
+        })
+    } else {
+        _movie = new Movie({
+            doctor: movieObj.doctor,
+            title: movieObj.title,
+            country: movieObj.country,
+            language: movieObj.language,
+            year: movieObj.year,
+            poster: movieObj.poster,
+            summary: movieObj.summary,
+            flash: movieObj.flash
+        })
+        _movie.save(function(err, movie) {
+            if (err) {
+                console.log(err)
+            }
+
+            res.redirect('/movie/' + movie._id);
         })
     }
 })
 
 //list page
 app.get('/admin/list', function(req, res) {
-    res.render('list', {
-        title: '列表页',
-        movies: [{
-            title: '冰川时代4',
-            _id: 3,
-            doctor: '麦克·特米尔',
-            country: 'USA',
-            year: 2012,
-            poster: 'http://r4.ykimg.com/0516000051B2E5BC6758391F6B05BFC4',
-            language: 'english',
-            flash: 'http://v.youku.com/v_show/id_XNDc0MTI3MTA4.html',
-            summary: '影片《冰川时代4》讲述的依然是那些生活在冰川时期的特殊动物“家庭”经历的冒险故事。那只永远追着松果的无敌贱，又苦逼的小松鼠奎特（克里斯·韦奇 配音）这次搞出了更大的事件，一个不小心让大陆板块四分五裂，使得猛犸象曼弗瑞德（雷·罗马诺 配音）、树懒希德（约翰·雷吉扎莫 配音）以及剑齿虎迪亚戈（丹尼斯·利瑞 配音）因此和家人、伙伴失散分离，在板块激烈的运动并分裂漂移后，只能使用一块流冰作为临...'
-        }]
+    Movie.fetch(function(err, movies) {
+        if (err) {
+            console.log(err)
+        }
+
+        res.render('list', {
+            title: '列表页',
+            movies: movies
+        })
     })
 });
+
+// delete movie 
+app.delete("/admin/list", function(req, res) {
+    var id = req.query.id;
+    console.log(id);
+    if (id) {
+        Movie.remove({ _id: id }, function(err, movie) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json({ success: 1 })
+            }
+        })
+    }
+})
